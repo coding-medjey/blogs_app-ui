ae95d436f3446cf2b62e329fc3199654707466f4
coding-medjey <spvaran123@gmail.com>
2024-08-15 09:40:17 +0530

    pending error handling

--- src/ListComments.jsx
+++ src/ListComments.jsx
@@ -5,7 +5,12 @@ export default ({ comments }) => {
   return (
     <div>
       <ul>
-        {comments.map(({ id, content }) => {
+        {comments.map(({ id, status, content }) => {
+          if (status === "rejected") {
+            content = "This Content has been removed";
+          } else if (status === "pending") {
+            content = "This comment has been awaiting for moderation";
+          }
           return <li key={id}>{content}</li>;
         })}
       </ul>
